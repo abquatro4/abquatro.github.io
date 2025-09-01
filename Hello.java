6a94c5ea2a60c6afa1ab858cd093408472a2323a
abquatro4 <abquatro4@gmail.com>
2025-09-01 16:06:22 -0600

    fix file name

---
+++ Hello.java
@@ -0,0 +1,20 @@
+public class Hello {
+
+    public static void main(String[] args) 
+    {
+        int a = 15;
+        System.out.println("Hello World");
+        System.out.print("My name is Albert, and I am ");
+        System.out.println(a + " years old.");
+
+        System.out.println("Parent, Albert 3rd.");
+        System.out.println("Parent, Laura.");
+        System.out.println("Sibling, Raphael.");
+        System.out.println("Sibling, Barnabas.");
+
+
+
+
+
+    }
+}
