4cca547823e90821a98b012e6a5b8285afbd1b59
abquatro4 <abquatro4@gmail.com>
2025-09-15 16:34:34 -0600

    Array Works!

--- array.java
+++ array.java
@@ -1,29 +1,34 @@
 // Code creator: Albert.
 // Code use: Gives the answer to whether number is odd or even up to 100.
+
 public class array {
+
     public static void main(String[] args) {
-       //Starting value.
-       int x = 0;
+        //Starting value.
+
         //intial number at which the sum starts
         int sum = 0;
         //While it equals less than one hundred the code will keep running until it equals one hundred.
         int arr[] = new int[100];
-         for (int u : arr) {
-            u = x;
-                //This is what adds one each time the loop runs.
-            x++;
-            int inputnumber = x;
-            if(inputnumber%2 == 0){
-            System.out.println(x + " is an even number");
-            
-            }else {
-            System.out.println(x + " is an odd number");
-          }
-        //Adds x to the sum each time the loop runs
-        sum = sum + x;
-       
+        for (int x = 0; x < arr.length; x++) {
+            arr[x] = x + 1;
         }
-         {
+        //While x is less than the array length it runs the loop.
+        for (int x = 0; x < arr.length; x++) {
+            //This is what tells the code that inputnumber is the array.
+            int inputnumber = arr[x];
+            //this is what shows whether or not the number is even.
+            if (inputnumber % 2 == 0) {
+                System.out.println(inputnumber + " is an even number");
+
+            } else {
+                System.out.println(inputnumber + " is an odd number");
+            }
+            sum = sum + inputnumber;
+            
+
+        }
+        {
 
         }
         //Prints the final sum of all loop iterations.
