a97ef85dab837e4ef368482778c4203d7a14982d
AB Quatro <abquatro4@gmail.com>
2025-09-29 15:09:31 -0600

    HorseProgramDone

--- HorseUser.java
+++ HorseUser.java
@@ -1,3 +1,16 @@
+/* 
+Code creator co-creator Albert and Kim Gross.
+Class: CTE Software devolpment 1.
+Instructor: Kim Gross.
+*/
 public class HorseUser {
-    
+    public static void main(String[] args) { //Creates a new main string
+        Horse horse1=new Horse("Buddy",2010); //Imports the information from Horse.java and puts in a horse and the year it was born.
+        System.out.println(horse1);
+        Horse horse2=new Horse("Dusty",2009);
+        System.out.println(horse2);
+        Horse horse3=new Horse("Annie",2018);
+        System.out.println(horse3);
+        
+    }
 }
