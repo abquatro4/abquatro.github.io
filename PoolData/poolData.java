cd63311b965c98f0a5eef5593163ce787af8d88c
AB Quatro <abquatro4@gmail.com>
2025-10-20 12:57:36 -0600

    PoolData Done

--- PoolData/poolData.java
+++ PoolData/poolData.java
@@ -2,26 +2,21 @@ package PoolData;
 //Creator Albert, stores data for a swimming pool.
 //Class: Software Devolpment 2025
 public class poolData {
-    int temp; //current pool temp
+    float temp; //current pool temp
     int LeafAmount; //Amount of leaves spotted in the pool
     String poolColor; //What color the pool is.
-    int tempCelsius; //Pool temp in Celsius.
+    String tempCelsius; //Pool temp in Celsius.
     public poolData(int currentTempFahrenheit, int LeafAmountNow, String currentColor) {
         temp=currentTempFahrenheit; //What temp is in the constructor
         LeafAmount=LeafAmountNow; //What leafAmount is in the constructor
         poolColor=currentColor; //what poolcolor is in the constructor
     };
     
-    public void tempInCelsius() {
-        int tempCelsius=temp-32*5/9; //Formula to convert the pool's fahrenheit temp to celsius.
-        
-        System.out.println("pool temp in Celsius"+tempCelsius);
+    public float tempInCelsius() {
+        float tempCelsius=(temp-32)*5/9; //Formula to convert the pool's fahrenheit temp to celsius.
+        return tempCelsius;
     }
-     public String toString (){
+    public String toColor() {
         return poolColor;
-    };    
-    public int toInt (){
-        return LeafAmount;
-        
-    }
+    };
 }
--- PoolData/poolDataWrapper.java
+++ PoolData/poolDataWrapper.java
@@ -1,7 +1,18 @@
+//Creator Albert
+//Class: Software and Programming Dev 1
 package PoolData;
 public class poolDataWrapper {
     public static void main(String[] args) { //Creates a new main string
-        poolData pool1=new poolData(32, 3,"Blue");
-        System.out.println(pool1);
+        poolData pool1=new poolData(42, 3,"Blue");
+        System.out.println("Pool color is: "+pool1.toColor()); //Returns color of the pool.
+        System.out.println("Pool temperature in Celsius: "+pool1.tempInCelsius()); //Returns temperature of the pool in Celsius.
+
+        poolData pool2=new poolData(84, 15,"Green");
+        System.out.println("Pool color is: "+pool2.toColor()); //Returns color of the pool.
+        System.out.println("Pool temperature in Celsius: "+pool2.tempInCelsius()); //Returns temperature of the pool in Celsius.
+
+        poolData pool3=new poolData(94, 42,"Greenish Blue");
+        System.out.println("Pool color is: "+pool3.toColor()); //Returns color of the pool.
+        System.out.println("Pool temperature in Celsius: "+pool3.tempInCelsius()); //Returns temperature of the pool in Celsius.
     }
 }
