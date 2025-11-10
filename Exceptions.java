b95e3d1b03aa1a00cfca20e2784288571bdafd80
AB Quatro <abquatro4@gmail.com>
2025-11-10 16:07:39 -0700

    Exceptions Created

---
+++ Exceptions.java
@@ -0,0 +1,13 @@
+//Creator Albert.
+
+//for CTE software development 1
+
+//instructor Mr Gross
+
+public class Exceptions {
+    
+    public static void main(String[] args) {
+        int array[] = new int[10];
+            System.out.println(array[10]);
+    }
+}
