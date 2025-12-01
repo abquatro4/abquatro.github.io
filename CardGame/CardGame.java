cb13732eafd24b47b0c33c65f06496800dd20490
AB Quatro <abquatro4@gmail.com>
2025-12-01 13:05:26 -0700

    test

--- CardGame/CardGame.java
+++ CardGame/CardGame.java
@@ -1,20 +1,12 @@
 package CardGame;
 
 public class CardGame {
-    int Hearts;
-    int Diamonds;
-    int Clubs;
-    int Spades;
-    CardGame(int Hearts, int Diamonds, int Clubs, int Spades) {
 
-        this.Hearts=Hearts;
-        this.Diamonds=Diamonds;
-        this.Clubs=Clubs;
-        this.Spades=Spades;
-    }
+ public static String Spades;
+ public static String Clubs;
+ public static String Diamonds;
+ public static String Hearts;
     
-    public int compareTo(CardGame a) { 
-        return  this.Diamonds - a.Diamonds;
-    }
+    
+   
 }
- 
--- CardGame/deck.java
+++ CardGame/deck.java
@@ -2,4 +2,15 @@ package CardGame;
 
 public class deck {
     
+    public static void main(String[] args) {
+        
+        
+        for (int i = 1; i <= 13; i++) {
+        System.out.println(CardGame.Hearts+" "+i);
+        System.out.println(CardGame.Diamonds+" "+i);
+        System.out.println(CardGame.Clubs+" "+i);
+        System.out.println(CardGame.Spades+" "+i);
+    }
 }
+    }
+    
