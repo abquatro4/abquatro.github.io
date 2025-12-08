44f2a657eb5e9736ac35c4dd1850122dc8a706cc
AB Quatro <abquatro4@gmail.com>
2025-12-08 13:16:43 -0700

    Fix

---
+++ CardGame/Card.java
@@ -0,0 +1,11 @@
+package CardGame;
+
+public class Card {
+
+    int value;
+    String suit;
+    public Card(int value, String suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+}
--- CardGame/deck.java
+++ CardGame/deck.java
@@ -1,21 +1,11 @@
 package CardGame;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
-import PoolData.ComparablePool;
-
-public class deck {
-
-
-    public static void main(String[] args) {
-        for (int i = 1; i <= 13; i++) {
-        System.out.println(CardGame.Hearts+" "+i);
-        System.out.println(CardGame.Diamonds+" "+i);
-        System.out.println(CardGame.Clubs+" "+i);
-        System.out.println(CardGame.Spades+" "+i);
+public class Deck {
+ArrayList<String> cards;
+    public Deck() {
+        cards=new ArrayList<Card>();
     }
-}
-    }
-    
+};
+Deck
